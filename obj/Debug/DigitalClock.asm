914b27149cc9a7af06710afed2bb3bf344f4e491
hussain ghazaly <you@example.com>
2020-12-27 14:59:39 +0200

    initial commit

---
+++ obj/Debug/DigitalClock.asm
@@ -0,0 +1,399 @@
+;--------------------------------------------------------
+; File Created by SDCC : free open source ANSI-C Compiler
+; Version 3.5.0 #9253 (Jun 20 2015) (MINGW32)
+; This file was generated Mon Jul 22 15:28:46 2019
+;--------------------------------------------------------
+; PIC port for the 14-bit core
+;--------------------------------------------------------
+	.file	"DigitalClock.c"
+	list	p=16f877a
+	radix dec
+	include "p16f877a.inc"
+;--------------------------------------------------------
+; config word(s)
+;--------------------------------------------------------
+	__config 0x3f32
+;--------------------------------------------------------
+; external declarations
+;--------------------------------------------------------
+	extern	_SW_Init
+	extern	_SW_GetState
+	extern	_SW_Update
+	extern	_TMR_Init
+	extern	_TMR_Start
+	extern	_TMR_CheckOverFlow
+	extern	_TMR_Stop
+	extern	_TMR_Update
+	extern	_DISP_Init
+	extern	_DISP_Update
+	extern	_TIM_Init
+	extern	_TIM_Update
+	extern	_TIM_GetTime
+	extern	_TIM_CompareTime
+	extern	_TIM_GetState
+	extern	_TIM_CopyTime
+	extern	_SSD_Init
+	extern	_SSD_Update
+	extern	_SSD_SetValue
+	extern	_SSD_GetValue
+	extern	_SSD_GetState
+	extern	_SSD_SetState
+	extern	_SSD_SetDotState
+	extern	_STATUSbits
+	extern	_PORTAbits
+	extern	_PORTBbits
+	extern	_PORTCbits
+	extern	_PORTDbits
+	extern	_PORTEbits
+	extern	_INTCONbits
+	extern	_PIR1bits
+	extern	_PIR2bits
+	extern	_T1CONbits
+	extern	_T2CONbits
+	extern	_SSPCONbits
+	extern	_CCP1CONbits
+	extern	_RCSTAbits
+	extern	_CCP2CONbits
+	extern	_ADCON0bits
+	extern	_OPTION_REGbits
+	extern	_TRISAbits
+	extern	_TRISBbits
+	extern	_TRISCbits
+	extern	_TRISDbits
+	extern	_TRISEbits
+	extern	_PIE1bits
+	extern	_PIE2bits
+	extern	_PCONbits
+	extern	_SSPCON2bits
+	extern	_SSPSTATbits
+	extern	_TXSTAbits
+	extern	_CMCONbits
+	extern	_CVRCONbits
+	extern	_ADCON1bits
+	extern	_EECON1bits
+	extern	_INDF
+	extern	_TMR0
+	extern	_PCL
+	extern	_STATUS
+	extern	_FSR
+	extern	_PORTA
+	extern	_PORTB
+	extern	_PORTC
+	extern	_PORTD
+	extern	_PORTE
+	extern	_PCLATH
+	extern	_INTCON
+	extern	_PIR1
+	extern	_PIR2
+	extern	_TMR1
+	extern	_TMR1L
+	extern	_TMR1H
+	extern	_T1CON
+	extern	_TMR2
+	extern	_T2CON
+	extern	_SSPBUF
+	extern	_SSPCON
+	extern	_CCPR1
+	extern	_CCPR1L
+	extern	_CCPR1H
+	extern	_CCP1CON
+	extern	_RCSTA
+	extern	_TXREG
+	extern	_RCREG
+	extern	_CCPR2
+	extern	_CCPR2L
+	extern	_CCPR2H
+	extern	_CCP2CON
+	extern	_ADRESH
+	extern	_ADCON0
+	extern	_OPTION_REG
+	extern	_TRISA
+	extern	_TRISB
+	extern	_TRISC
+	extern	_TRISD
+	extern	_TRISE
+	extern	_PIE1
+	extern	_PIE2
+	extern	_PCON
+	extern	_SSPCON2
+	extern	_PR2
+	extern	_SSPADD
+	extern	_SSPSTAT
+	extern	_TXSTA
+	extern	_SPBRG
+	extern	_CMCON
+	extern	_CVRCON
+	extern	_ADRESL
+	extern	_ADCON1
+	extern	_EEDATA
+	extern	_EEADR
+	extern	_EEDATH
+	extern	_EEADRH
+	extern	_EECON1
+	extern	_EECON2
+	extern	__sdcc_gsinit_startup
+;--------------------------------------------------------
+; global declarations
+;--------------------------------------------------------
+	global	_main
+	global	_flag
+
+	global PSAVE
+	global SSAVE
+	global WSAVE
+	global STK12
+	global STK11
+	global STK10
+	global STK09
+	global STK08
+	global STK07
+	global STK06
+	global STK05
+	global STK04
+	global STK03
+	global STK02
+	global STK01
+	global STK00
+
+sharebank udata_ovr 0x0070
+PSAVE	res 1
+SSAVE	res 1
+WSAVE	res 1
+STK12	res 1
+STK11	res 1
+STK10	res 1
+STK09	res 1
+STK08	res 1
+STK07	res 1
+STK06	res 1
+STK05	res 1
+STK04	res 1
+STK03	res 1
+STK02	res 1
+STK01	res 1
+STK00	res 1
+
+;--------------------------------------------------------
+; global definitions
+;--------------------------------------------------------
+;--------------------------------------------------------
+; absolute symbol definitions
+;--------------------------------------------------------
+;--------------------------------------------------------
+; compiler-defined variables
+;--------------------------------------------------------
+;--------------------------------------------------------
+; initialized data
+;--------------------------------------------------------
+
+ID_DigitalClock_0	idata
+_flag
+	db	0x00
+
+;--------------------------------------------------------
+; overlayable items in internal ram 
+;--------------------------------------------------------
+;	udata_ovr
+;--------------------------------------------------------
+; reset vector 
+;--------------------------------------------------------
+STARTUP	code 0x0000
+	nop
+	pagesel __sdcc_gsinit_startup
+	goto	__sdcc_gsinit_startup
+;--------------------------------------------------------
+; code
+;--------------------------------------------------------
+code_DigitalClock	code
+;***
+;  pBlock Stats: dbName = M
+;***
+;entry:  _main	;Function start
+; 2 exit points
+;has an exit
+;functions called:
+;   _SW_Init
+;   _DISP_Init
+;   _TIM_Init
+;   _TMR_Init
+;   _TMR_Start
+;   _SW_Init
+;   _DISP_Init
+;   _TIM_Init
+;   _TMR_Init
+;   _TMR_Start
+;; Starting pCode block
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2760:genLabel *{*
+;; ***	genLabel  2763
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2340:genFunction *{*
+;; ***	genFunction  2342 curr label offset=0previous max_key=0 
+_main	;Function start
+; 2 exit points
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	15; "DigitalClock.c"	SW_Init();
+	PAGESEL	_SW_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_SW_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	16; "DigitalClock.c"	DISP_Init();
+	PAGESEL	_DISP_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_DISP_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	17; "DigitalClock.c"	TIM_Init();
+	PAGESEL	_TIM_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_TIM_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	18; "DigitalClock.c"	TMR_Init();
+	PAGESEL	_TMR_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_TMR_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	19; "DigitalClock.c"	TMR_Start();
+	PAGESEL	_TMR_Start
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_TMR_Start
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6686:genPointerSet *{*
+;; ***	genPointerSet  6687
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6505:genNearPointerSet *{*
+;; ***	genNearPointerSet  6506
+;;	613
+;;	aopForRemat 392
+;;	418: rname _TRISCbits, val 0, const = 0
+;; ***	genNearPointerSet  6521
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_TRISCbits
+;; 	line = 6523 result AOP_PCODE=_TRISCbits, size=2, left -=-, size=0, right AOP_LIT=0x00, size=1
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6249:genPackBits *{*
+;; ***	genPackBits  6250
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_TRISCbits
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6275:genPackBits
+	.line	21; "DigitalClock.c"	TRISC7 = 0;
+	BANKSEL	_TRISCbits
+	BCF	_TRISCbits,7
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6686:genPointerSet *{*
+;; ***	genPointerSet  6687
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6505:genNearPointerSet *{*
+;; ***	genNearPointerSet  6506
+;;	613
+;;	aopForRemat 392
+;;	418: rname _PORTCbits, val 0, const = 0
+;; ***	genNearPointerSet  6521
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_PORTCbits
+;; 	line = 6523 result AOP_PCODE=_PORTCbits, size=2, left -=-, size=0, right AOP_LIT=0x00, size=1
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6249:genPackBits *{*
+;; ***	genPackBits  6250
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_PORTCbits
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6275:genPackBits
+	.line	22; "DigitalClock.c"	RC7 = 0;
+	BANKSEL	_PORTCbits
+	BCF	_PORTCbits,7
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2760:genLabel *{*
+;; ***	genLabel  2763
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6759:genIfx *{*
+;; ***	genIfx  6760
+;; ***	aopForSym 315
+;;	327 sym->rname = _flag, size = 1
+;; ***	pic14_toBoolean  1515
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1522:pic14_toBoolean
+_00105_DS_
+	.line	27; "DigitalClock.c"	while (flag == 0);
+	MOVLW	0x00
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1533:pic14_toBoolean
+;;	1009
+;;	1028  _flag   offset=0
+	BANKSEL	_flag
+	IORWF	_flag,W
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6813:genIfx
+	BTFSC	STATUS,2
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6814:genIfx
+;; ***	popGetLabel  key=1, label offset 4
+	GOTO	_00105_DS_
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6927:genAssign *{*
+;; ***	genAssign  6928
+;; ***	aopForSym 315
+;;	327 sym->rname = _flag, size = 1
+;; 	line = 6937 result AOP_DIR=_flag, size=1, left -=-, size=0, right AOP_LIT=0x00, size=1
+;; ***	genAssign  7023
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:7035:genAssign
+;;	1009
+;;	1028  _flag   offset=0
+	.line	29; "DigitalClock.c"	flag = 0;
+	CLRF	_flag
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2778:genGoto *{*
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2780:genGoto
+;; ***	popGetLabel  key=1, label offset 4
+	GOTO	_00105_DS_
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2760:genLabel *{*
+;; ***	genLabel  2763
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2528:genEndFunction *{*
+;; ***	genEndFunction  2530
+	RETURN	
+; exit point of _main
+
+
+;	code size estimation:
+;	   14+   13 =    27 instructions (   80 byte)
+
+	end
---
+++ obj/Debug/DigitalClock.p
@@ -0,0 +1,172 @@
+pcode dump
+
+
+	New pBlock
+
+internal pblock, dbName =M
+;; Starting pCode block
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2760:genLabel *{*
+;; ***	genLabel  2763
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2340:genFunction *{*
+;; ***	genFunction  2342 curr label offset=0previous max_key=0 
+_main	;Function start
+; 2 exit points
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	15; "DigitalClock.c"	SW_Init();
+	PAGESEL	_SW_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_SW_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	16; "DigitalClock.c"	DISP_Init();
+	PAGESEL	_DISP_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_DISP_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	17; "DigitalClock.c"	TIM_Init();
+	PAGESEL	_TIM_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_TIM_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	18; "DigitalClock.c"	TMR_Init();
+	PAGESEL	_TMR_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_TMR_Init
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2086:genCall *{*
+;; ***	genCall  2088
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1801:saveRegisters *{*
+;; ***	saveRegisters  1803
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2173:genCall
+	.line	19; "DigitalClock.c"	TMR_Start();
+	PAGESEL	_TMR_Start
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2175:genCall
+	CALL	_TMR_Start
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2181:genCall
+	PAGESEL	$
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1845:unsaveRegisters *{*
+;; ***	unsaveRegisters  1847
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6686:genPointerSet *{*
+;; ***	genPointerSet  6687
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6505:genNearPointerSet *{*
+;; ***	genNearPointerSet  6506
+;;	613
+;;	aopForRemat 392
+;;	418: rname _TRISCbits, val 0, const = 0
+;; ***	genNearPointerSet  6521
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_TRISCbits
+;; 	line = 6523 result AOP_PCODE=_TRISCbits, size=2, left -=-, size=0, right AOP_LIT=0x00, size=1
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6249:genPackBits *{*
+;; ***	genPackBits  6250
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_TRISCbits
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6275:genPackBits
+	.line	21; "DigitalClock.c"	TRISC7 = 0;
+	BCF	_TRISCbits,7
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6686:genPointerSet *{*
+;; ***	genPointerSet  6687
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6505:genNearPointerSet *{*
+;; ***	genNearPointerSet  6506
+;;	613
+;;	aopForRemat 392
+;;	418: rname _PORTCbits, val 0, const = 0
+;; ***	genNearPointerSet  6521
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_PORTCbits
+;; 	line = 6523 result AOP_PCODE=_PORTCbits, size=2, left -=-, size=0, right AOP_LIT=0x00, size=1
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6249:genPackBits *{*
+;; ***	genPackBits  6250
+;;	833: aopGet AOP_PCODE type PO_IMMEDIATE
+;;	_PORTCbits
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6275:genPackBits
+	.line	22; "DigitalClock.c"	RC7 = 0;
+	BCF	_PORTCbits,7
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2760:genLabel *{*
+;; ***	genLabel  2763
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6759:genIfx *{*
+;; ***	genIfx  6760
+;; ***	aopForSym 315
+;;	327 sym->rname = _flag, size = 1
+;; ***	pic14_toBoolean  1515
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1522:pic14_toBoolean
+_00105_DS_
+	.line	27; "DigitalClock.c"	while (flag == 0);
+	MOVLW	0x00
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:1533:pic14_toBoolean
+;;	1009
+;;	1028  _flag   offset=0
+	IORWF	_flag,W
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6813:genIfx
+	BTFSC	STATUS,2
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6814:genIfx
+;; ***	popGetLabel  key=1, label offset 4
+	GOTO	_00105_DS_
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:6927:genAssign *{*
+;; ***	genAssign  6928
+;; ***	aopForSym 315
+;;	327 sym->rname = _flag, size = 1
+;; 	line = 6937 result AOP_DIR=_flag, size=1, left -=-, size=0, right AOP_LIT=0x00, size=1
+;; ***	genAssign  7023
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:7035:genAssign
+;;	1009
+;;	1028  _flag   offset=0
+	.line	29; "DigitalClock.c"	flag = 0;
+	CLRF	_flag
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2778:genGoto *{*
+;; >>> /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2780:genGoto
+;; ***	popGetLabel  key=1, label offset 4
+	GOTO	_00105_DS_
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2760:genLabel *{*
+;; ***	genLabel  2763
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2316:resultRemat *{*
+;;; /home/sdcc-builder/build/sdcc-build/orig/sdcc/src/pic14/gen.c:2528:genEndFunction *{*
+;; ***	genEndFunction  2530
+	RETURN	
+; exit point of _main
